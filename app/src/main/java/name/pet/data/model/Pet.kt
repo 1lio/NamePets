d29755d7c73c7502d1440bd56a285df6f3cc1bb5
ViacheslavSukhov <svo_efremov@mail.ru>
2020-08-14 10:42:45 +0300

    Ad Room annotations

--- app/src/main/java/name/pet/data/model/Pet.kt
+++ app/src/main/java/name/pet/data/model/Pet.kt
@@ -1,9 +1,20 @@
 package name.pet.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class Pet(
 
+    @PrimaryKey
     val id: String,
+
     val type: String = "cat",       // cat or dog
+
+    @ColumnInfo(name = "name")
     val name: String = "Васька",
+
+    @ColumnInfo(name = "gender")
     val gender: Boolean = true      // true is male other female
 )
