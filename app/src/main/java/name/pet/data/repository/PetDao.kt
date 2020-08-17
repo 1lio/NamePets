64c0824b5cbd617775a18145e8897056a22ae5b6
ViacheslavSukhov <svo_efremov@mail.ru>
2020-08-17 10:07:00 +0300

    Update db

--- app/src/main/java/name/pet/data/repository/PetDao.kt
+++ app/src/main/java/name/pet/data/repository/PetDao.kt
@@ -7,10 +7,10 @@ import name.pet.data.model.Pet
 @Dao
 interface PetDao {
 
-    @Query("SELECT cat FROM gender WHERE :gender")
+    @Query("SELECT cats FROM gender WHERE :gender")
     fun getCats(gender: Int = 1): List<Pet>
 
-    @Query("SELECT dog FROM gender WHERE :gender")
+    @Query("SELECT dogs FROM gender WHERE :gender")
     fun getDogs(gender: Int = 1): List<Pet>
 
 }
--- app/src/main/java/name/pet/data/repository/PetRepository.kt
+++ app/src/main/java/name/pet/data/repository/PetRepository.kt
@@ -2,17 +2,18 @@ package name.pet.data.repository
 
 import android.content.Context
 import androidx.room.Room
+import java.io.File
 
 class PetRepository(context: Context) {
 
     private val db = Room
         .databaseBuilder(context, AppDatabase::class.java, "pets")
+        .createFromFile(File("raw/names.db"))
         .build()
 
+    fun getRandomName(type: Int, gender: Int): String {
 
-    fun getRandomName(type: Boolean, gender: Int): String {
-
-        if (type) db.petDao().getCats(gender) else db.petDao().getDogs(gender)
+        if (type == 1) db.petDao().getCats(gender) else db.petDao().getDogs(gender)
 
         return "Васька"
     }
