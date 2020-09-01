b7d91a599d7a7642c14f4dbee4608217413dd32e
ViacheslavSukhov <svo_efremov@mail.ru>
2020-09-01 10:16:50 +0300

    Configure AppDatabase

--- app/src/main/java/name/pet/data/repository/AppDatabase.kt
+++ app/src/main/java/name/pet/data/repository/AppDatabase.kt
@@ -1,10 +1,28 @@
 package name.pet.data.repository
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import name.pet.data.model.Pet
 
-@Database(entities = [Pet::class], version = 1)
+@Database(version = 100, entities = [Pet::class])
 abstract class AppDatabase : RoomDatabase() {
+
+    companion object {
+
+        private const val DB_NAME = "names"
+        private const val DB_DIR = "/databases/names.db"
+
+        fun getInstance(context: Context): AppDatabase {
+            return Room
+                .databaseBuilder(context, AppDatabase::class.java, DB_NAME)
+                .allowMainThreadQueries()
+                .createFromAsset(DB_DIR)
+                .build()
+        }
+
+    }
+
     abstract fun petDao(): PetDao
 }
