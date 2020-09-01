8c56badc5312b1ff47fb849072016f0fcd6f3ce6
ViacheslavSukhov <svo_efremov@mail.ru>
2020-09-01 10:17:09 +0300

    Update PetDao

--- app/src/main/java/name/pet/data/repository/PetDao.kt
+++ app/src/main/java/name/pet/data/repository/PetDao.kt
@@ -7,10 +7,10 @@ import name.pet.data.model.Pet
 @Dao
 interface PetDao {
 
-    @Query("SELECT cats FROM gender WHERE :gender")
+    @Query("SELECT * FROM cats WHERE gender = :gender")
     fun getCats(gender: Int = 1): List<Pet>
 
-    @Query("SELECT dogs FROM gender WHERE :gender")
-    fun getDogs(gender: Int = 1): List<Pet>
+  /*  @Query("SELECT * FROM dogs WHERE gender = :gender")
+    fun getDogs(gender: Int = 1): List<PetDog>*/
 
 }
