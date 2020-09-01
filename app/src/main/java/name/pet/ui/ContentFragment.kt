6814c3307d884bf8cf35f6fccfba812e34e80bb7
ViacheslavSukhov <svo_efremov@mail.ru>
2020-09-01 10:18:11 +0300

    Update pet query

--- app/src/main/java/name/pet/ui/ContentFragment.kt
+++ app/src/main/java/name/pet/ui/ContentFragment.kt
@@ -1,41 +1,30 @@
 package name.pet.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.room.Room
 import kotlinx.android.synthetic.main.content.*
 import name.pet.R
 import name.pet.data.repository.AppDatabase
-import java.io.File
+import kotlin.random.Random
 
-class ContentFragment : Fragment() {
+class ContentFragment : Fragment(R.layout.content) {
 
     private lateinit var db: AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        db = Room
-            .databaseBuilder(requireContext().applicationContext, AppDatabase::class.java, "names")
-            .createFromFile(File("raw/names.db"))
-            .allowMainThreadQueries()
-            .build()
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, state: Bundle?): View? {
-        super.onCreateView(inflater, container, state)
-        return inflater.inflate(R.layout.content, container, false)
+        db = AppDatabase.getInstance(context!!.applicationContext)
     }
 
     private fun getRandomName(type: Int, gender: Int): String {
-        if (type == 1) db.petDao().getCats(gender) else db.petDao().getDogs(gender)
-        return "Васька"
-    }
 
-    // private val repo = PetRepository(context!!)
+        val list = db.petDao().getCats(gender)
+        val random = Random.nextInt(0, list.size - 1)
+
+        val pet = list[random]
+        return pet.name
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
