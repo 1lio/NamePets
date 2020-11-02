320bdbf0a53710e6a8bddc8ec73491e23abebc6d
1lio <svoefremov@yandex.ru>
2020-11-02 20:37:52 +0300

    Minimal changes

--- app/src/main/java/name/pet/ui/ContentFragment.kt
+++ app/src/main/java/name/pet/ui/ContentFragment.kt
@@ -1,12 +1,10 @@
 package name.pet.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.content.*
 import name.pet.R
-import name.pet.data.model.Pet
 import name.pet.data.repository.AppDatabase
 import kotlin.random.Random
 
@@ -16,22 +14,20 @@ class ContentFragment : Fragment(R.layout.content) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        db = AppDatabase.getInstance(context!!.applicationContext)
+        db = AppDatabase.getInstance(requireContext())
     }
 
-    private fun getRandomName(animal: Int, gender: Int): String {
-        val listPet = if (animal == 0) {
-            val catsDao = db.catsDao()
-            catsDao.getCats(gender)
+    private fun getRandomName(petType: Int, petGender: Int): String {
+
+        val listPet = if (petType == 0) {
+            db.catsDao().getCats(petGender)
         } else {
-            val dogsDao = db.dogsDao()
-            dogsDao.getDogs(gender)
+            db.dogsDao().getDogs(petGender)
         }
 
         val random = Random.nextInt(0, listPet.size)
 
-        val pet = listPet[random]
-        return pet.name
+        return listPet[random].name
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -39,10 +35,8 @@ class ContentFragment : Fragment(R.layout.content) {
 
         btnPetName.setOnClickListener {
             val randomName = getRandomName(
-                if (cat.isChecked) 0
-                else 1,
-                if (male.isChecked) 1
-                else 0
+                if (cat.isChecked) 0 else 1,
+                if (male.isChecked) 1 else 0
             )
             petName.text = randomName
         }
